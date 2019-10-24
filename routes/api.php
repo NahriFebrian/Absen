29c8c14b5ee4fa52e6946fc6fdc99d2e0b9a7574
NahriFebrian <nahrifebrian@gmail.com>
2019-10-24 15:52:49 +0700

    add Api.php

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('auth/register','AuthController@register');
+Route::post('auth/login','AuthController@login');
+Route::get('users','UserController@users');
+Route::get('users/profile','UserController@profile')->middleware('auth:api');
+Route::get('users/{id}','UserController@profileById')->middleware('auth:api');
+Route::post('absen','PostController@add')->middleware('auth:api');
+Route::put('absen/{absen}','AbsentController@update')->middleware('auth:api');
+Route::delete('absen/{absen}','AbsenController@delete')->middleware('auth:api');
+Route::get('status','StatusController@status');
+Route::post('status','StatusController@add')->middleware('auth:api');
+Route::put('status/{status}','StatusController@update')->middleware('auth:api');
+Route::delete('status/{status}','StatusController@delete')->middleware('auth:api');
